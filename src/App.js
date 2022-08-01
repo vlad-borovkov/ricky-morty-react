d9c26d5ee957640197a6076585c5f527fbfee635
Vlad <borovkov-vlad@ya.ru>
2022-08-02 01:24:51 +0500

    подготовка среды

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from "react";
+import "./index.css";
+
+const App = () => {
+  
+    return(
+      <div className="App">
+        <h1> Hello, World!!!! </h1>
+      </div>
+    );
+  }
+
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+        publicPath: ''
+  },
+  devServer: {
+    static: path.resolve(__dirname, './dist'), 
+    compress: true, 
+    port: 3000,
+    hot: false,
+    open: true 
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), 
+    new HtmlWebpackPlugin({
+    template: './src/index.html'
+  }), 
+    new CleanWebpackPlugin()]
+};
